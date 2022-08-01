ae9c09e909fb9f2ff978f20b1394baaf2424efee
Raul Mercado <raulmercadox@gmail.com>
2022-08-01 00:28:59 -0500

    First upload

---
+++ index.js
@@ -0,0 +1,29 @@
+function add1Home() {
+    let counterHome = document.getElementById('counterHome');
+    counterHome.textContent = Number(counterHome.textContent) + 1;
+}
+
+function add2Home() {
+    let counterHome = document.getElementById('counterHome');
+    counterHome.textContent = Number(counterHome.textContent) + 2;
+}
+
+function add3Home() {
+    let counterHome = document.getElementById('counterHome');
+    counterHome.textContent = Number(counterHome.textContent) + 3;
+}
+
+function add1Guest() {
+    let counterGuest = document.getElementById('counterGuest');
+    counterGuest.textContent = Number(counterGuest.textContent) + 1;
+}
+
+function add2Guest() {
+    let counterGuest = document.getElementById('counterGuest');
+    counterGuest.textContent = Number(counterGuest.textContent) + 2;
+}
+
+function add3Guest() {
+    let counterGuest = document.getElementById('counterGuest');
+    counterGuest.textContent = Number(counterGuest.textContent) +3;
+}
